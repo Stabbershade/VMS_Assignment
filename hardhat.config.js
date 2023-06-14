36fa0ceda3d589bc228fb1e5328ad687ba903444
Stabbershade <Stabbershade@hotmail.com>
2023-06-14 20:44:33 +0800

    Added .env examples, Configured Sepolia testnet newtwork to hardhat.config.js and Written deploy.js script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhart",
   solidity: "0.8.18",
+  networks:{
+    sepolia:{
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY] 
+    }
+  }
 };
