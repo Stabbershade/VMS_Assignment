a15066dfaf01c5a2b171137f7e60cde022d3e22d
Stabbershade <Stabbershade@hotmail.com>
2023-06-14 20:58:21 +0800

    Added artifacts and cache folder to .gitignore and fixed deploy.js with deafult testcase

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,14 @@
 const { ethers } = require("hardhat")
+const DeafultTest = ["John","Sam","Bob"]
 
 async function main(){
+
   const contractFactory = await ethers.getContractFactory("Voting")
   console.log("Deploying Contract...")
-  const VotingContract = await contractFactory.deploy()
+  const VotingContract = await contractFactory.deploy(DeafultTest)
   await VotingContract.waitForDeployment()
-  console.log(`Contract address:${simpleStorage.target}`)
-  
+  console.log(`Contract address:${VotingContract.target}`)
+
 }
 
 main().then(()=>process.exit(0)).catch((error) => {
