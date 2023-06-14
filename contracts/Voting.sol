75610d0f1739e81b23c5e41cd493ec6da910f59b
Stabbershade <Stabbershade@hotmail.com>
2023-06-14 23:51:26 +0800

    Major change: Removed Voter Struct ,renamed functions to 1)Vote() , 2)getCandidate() ,3)getResult() and improved getResult logic

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,94 +1,78 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-
-//A voting management system that enables users to vote for their favorite candidate in an election. 
+//A voting management system that enables users to vote for their favorite candidate in an election.
 //The smart contract would handle vote casting, tallying, and result reporting.
 
-contract Voting{
+contract Voting {
 
-    struct Voter {
-        uint8 exist;
-        bool hasVoted;
-        string votedFor;
-    }
-    
-    struct Candidate{
+    struct Candidate {
         uint8 exist;
         string name;
-        uint voteCount;
+        uint256 voteCount;
     }
 
     address public immutable owner;
-    mapping (address => Voter) public voters;
-    mapping (string => Candidate) private candidates;
-    string[] private storeCandidate; 
+    mapping(address => bool) public hasVoted;
+    mapping(string => Candidate) private candidates;
+    string[] private candidateStore;
 
-    constructor(string[] memory _candidiate){
+    event VoteCasted(string indexed candidate);
+
+    constructor(string[] memory _candidiate) {
         owner = msg.sender;
-        for(uint i = 0 ; i < _candidiate.length; i++){
-            candidates[_candidiate[i]] = Candidate({exist: 1 , name: _candidiate[i] , voteCount: 0});
-            storeCandidate = _candidiate;
+        for (uint256 i = 0; i < _candidiate.length; i++) {
+            candidates[_candidiate[i]] = Candidate({
+                exist: 1,
+                name: _candidiate[i],
+                voteCount: 0
+            });
+            candidateStore = _candidiate;
         }
     }
 
-    function voteForCandidate(string memory candidateName) public VoteOnce nameExist(candidateName){
+    function Vote(string memory candidateName) public VoteOnce nameExist(candidateName){
         candidates[candidateName].voteCount += 1;
-        voters[msg.sender].votedFor = candidateName;
-        voters[msg.sender].hasVoted = true;
+        hasVoted[msg.sender] = true;
+        emit VoteCasted(candidateName);
     }
 
-    function getSpecficResult(string memory candidateName) public nameExist(candidateName) view returns(uint) {
+    function getCandidate(string memory candidateName) public view nameExist(candidateName) returns (uint256) {
         return candidates[candidateName].voteCount;
     }
 
-    function Winner() public view returns(string memory){
-        uint min = 0;
-        string memory winner = " ";
-        for(uint i = 0 ; i < storeCandidate.length; i++){
-            if(getSpecficResult(storeCandidate[i]) > min){
-                min = getSpecficResult(storeCandidate[i]);
-                winner = storeCandidate[i];
-            }
-            else if(getSpecficResult(storeCandidate[i]) == min){
-                winner = string.concat(winner,storeCandidate[i]);
+    function getResult() public view returns (string[] memory) {
+        uint256 min = 0;
+        string[] memory winner = new string[](candidateStore.length);
+        uint counter = 0;
+        for (uint256 i = 0; i < candidateStore.length; i++) {
+            if (getCandidate(candidateStore[i]) > min) {
+                min = getCandidate(candidateStore[i]);
+                winner = new string[](candidateStore.length);
+                counter = 0;
+                winner[counter] = candidateStore[i];
+                counter++;
+            } else if (getCandidate(candidateStore[i]) == min) {
+                winner[counter++] = candidateStore[i];
             }
         }
         return winner;
     }
 
-    modifier VoteOnce{
-        if(!VoterExists(msg.sender)){
-            voters[msg.sender] = Voter({exist: 1 , hasVoted: false , votedFor: ""});
-        }
-        else{
-            require(voters[msg.sender].hasVoted == false , "You have voted already!");
-        }
+    modifier VoteOnce() {
+        require(!hasVoted[msg.sender], "You have already voted.");
         _;
     }
 
-    modifier nameExist(string memory candidateName){
-        require(CandidateExists(candidateName) == true , "No such candidate exist");
+    modifier nameExist(string memory candidateName) {
+        require(CandidateExists(candidateName) == true,"No such candidate exist");
         _;
-    }
-
-    function VoterExists(address key) internal view returns (bool) {
-        if(voters[key].exist > 0){
-            return true;
-        }
-        return false;
     }
 
     function CandidateExists(string memory key) internal view returns (bool) {
-        if(candidates[key].exist > 0){
+        if (candidates[key].exist > 0) {
             return true;
         }
         return false;
     }
-
-
-
-
-
-
 }
