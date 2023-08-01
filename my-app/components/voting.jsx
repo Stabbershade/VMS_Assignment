1ca13c7551d3dc275b4eb1f17b8974092625bb60
Wei Jie <Stabbershade@hotmail.com>
2023-08-01 18:09:59 +0800

    Added Approute Components, installed ethers package on nextJs, Customised Frontend and optimized backend

---
+++ my-app/components/voting.jsx
@@ -0,0 +1,26 @@
+const Voting = ({input,setInput,voting}) => {
+    return(
+        <div>
+            <h2 className='text-4xl bolder text-[#1d4ed8] pb-8'>
+            Welcome to VMS_Assignment
+            </h2>
+            <div className='py-3 text-[#020617]'>
+            Voting for...
+            </div>
+                <form className='flex items-center justify-center'>
+                    <input
+                    className='rounded-[10px] w-full p-[10px] border-none outline-none bg-[#031956] text-white mb-[10px]'
+                    placeholder='Candidates Name'
+                    value = {input}
+                    onChange={e => setInput(e.target.value)}
+                    />
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded ml-2 mb-2.5"
+                    onClick={voting}>
+                        Vote 
+                    </button>
+                </form>
+        </div>
+    )
+}
+
+export default Voting
---
+++ my-app/config.js
@@ -0,0 +1,2 @@
+//Contract Address that was created
+export const VotingContractAddress = '0x75df35Fad6132BABe7A5718e65D9Ccc98d611eF0'
